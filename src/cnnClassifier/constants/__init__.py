703d9704c6b20108e55a65048480d0670d479fd1
Sai kishan <saikishan2452003@gmail.com>
2026-01-04 16:27:19 +0530

    model training added

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,8 @@
+
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+# Absolute project root
+ROOT_DIR = Path(__file__).resolve().parents[3]
+
+CONFIG_FILE_PATH = ROOT_DIR / "config" / "config.yaml"
+PARAMS_FILE_PATH = ROOT_DIR / "params.yaml"
